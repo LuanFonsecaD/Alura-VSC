893af72aa26c3aaccae744bcedf3275668bacc00
Luan Fonseca <luanjeremias2@gmail.com>
2023-02-22 10:08:44 -0300

    novo commit

---
+++ 2type-bool.js
@@ -0,0 +1,19 @@
+// Tipo booleano
+
+const num1 = 5;
+const num2 = 10;
+const num3 = 5;
+
+// Comparação [===]:
+
+console.log(num1 === num2); //false
+console.log(num1 === num3); //true
+
+// Comparação entre strings
+
+const texto1 = "Alura";
+const texto2 = "Boti";
+const texto3 = "alura";
+
+console.log(texto1 === texto2); //false
+console.log(texto1 === texto3); //false
